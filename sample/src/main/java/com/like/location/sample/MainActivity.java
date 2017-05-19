463d64373726254311d558ddcdaf69aafdb6a3ed
like5188 <260864866@qq.com>
2017-05-19 17:21:57 +0800

    bug

--- sample/src/main/java/com/like/location/sample/MainActivity.java
+++ sample/src/main/java/com/like/location/sample/MainActivity.java
@@ -1,13 +1,37 @@
 package com.like.location.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baidu.location.BDLocation;
+import com.like.location.LocationUtils;
+import com.like.location.MyLocationListener;
+import com.like.logger.Logger;
 
 public class MainActivity extends AppCompatActivity {
+    private LocationUtils mLocationUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initLocation();
+    }
+
+    /**
+     * 初始化百度定位
+     */
+    public void initLocation() {
+        mLocationUtils = new LocationUtils(this, new MyLocationListener() {
+            @Override
+            public void onReceiveLocation(BDLocation location) {
+                super.onReceiveLocation(location);
+                if (location != null) {
+                    Logger.i(location.getCity());
+                }
+                mLocationUtils.stop();
+            }
+        });
+        mLocationUtils.start();
     }
 }
