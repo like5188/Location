3312fab5a30a0ac742931f3fb3931094621a2c09
PC-20180923QFUS\Administrator <liuhongyun@adtech.com.cn>
2019-01-08 11:18:55 +0800

    新版

--- location/src/main/java/com/like/location/TraceUtils.kt
+++ location/src/main/java/com/like/location/TraceUtils.kt
@@ -51,7 +51,7 @@ class TraceUtils(private val context: Context,
 
     private val mSequenceGenerator = AtomicInteger()
     private val locRequest: LocRequest by lazy { LocRequest(serviceId) }
-    // 是否需要对象存储服务，默认为：false，关闭对象存储服务。注：鹰眼 Android SDK v3.0以上版本支持随轨迹上传图像等对象数据，若需使用此功能，该参数需设为 true，且需导入bos-android-sdk-1.0.2.jar。
+    // 是否需要对象存储服务，默认为：false，关闭对象存储服务。注：鹰眼 Android SDK v3.0以上版本支持随轨迹上传图像等对象数据，若需使用此功能，该参数需设为 true，且需导入bos-android-sdk-1.0.3.jar。
     private val isNeedObjectStorage = false
     // 初始化轨迹服务
     private val mTrace: Trace by lazy { Trace(serviceId, myEntityName, isNeedObjectStorage) }
