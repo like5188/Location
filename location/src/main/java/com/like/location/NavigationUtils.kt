5c120a658a62b62af4a45ce59b7eedd15cfd2094
PC-20180923QFUS\Administrator <liuhongyun@adtech.com.cn>
2019-01-09 11:30:51 +0800

    新版

--- location/src/main/java/com/like/location/NavigationUtils.kt
+++ location/src/main/java/com/like/location/NavigationUtils.kt
@@ -15,11 +15,11 @@ object NavigationUtils {
 
     fun navigation(context: Context, endlatitude: Double, endlongitude: Double) {
         when {
-            isAvilible(context, BAIDU_MAP_APP) -> // 启动百度地图导航
+            isAvailable(context, BAIDU_MAP_APP) -> // 启动百度地图导航
                 context.startActivity(Intent().apply {
                     this.data = Uri.parse("baidumap://map/direction?destination=$endlatitude,$endlongitude")
                 })
-            isAvilible(context, GAODE_MAP_APP) -> // 启动高德地图导航
+            isAvailable(context, GAODE_MAP_APP) -> // 启动高德地图导航
                 context.startActivity(Intent().apply {
                     this.data = Uri.parse("amapuri://route/plan/?dlat=$endlatitude&dlon=$endlongitude&dev=0&t=0")
                 })
@@ -30,7 +30,7 @@ object NavigationUtils {
     /**
      * 验证各种导航地图是否安装
      */
-    private fun isAvilible(context: Context, packageName: String): Boolean {
+    private fun isAvailable(context: Context, packageName: String): Boolean {
         //获取所有已安装程序的包信息
         val packageInfos = context.packageManager.getInstalledPackages(0)
         return packageInfos?.any { it.packageName == packageName } ?: false
